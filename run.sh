36514513c6ce40017a6f71adca2a3ad78f401424
k2sebeom <moses97@gmail.com>
2025-04-19 07:04:05 +0000

    feat: Adjust max upload size

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-streamlit run app.py --server.address 0.0.0.0 -- -c config.yaml
+streamlit run app.py \
+    --server.address 0.0.0.0 \
+    --server.maxUploadSize 1000000 \
+    -- -c config.yaml
